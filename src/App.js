7f5680a72fcc975f2318e69eba0611a41dd9ef51
User <vargas.mattykun@gmail.com>
2025-07-05 13:55:53 +0800

    Added Channel Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SideBar from './components/SideBar';
+import ChannelBar from './components/ChannelBar';
 
 function App() {
   return (
     <div className="flex">
+      <ChannelBar />
       <SideBar/>
     </div>
   );
---
+++ src/components/ChannelBar.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { BsHash } from 'react-icons/bs';
+import { FaChevronDown, FaChevronRight, FaPlus } from 'react-icons/fa';
+
+const ChannelBar = () => {
+    return (
+        <div className="channel-bar shadow-lg">
+
+        </div>
+    )
+}
